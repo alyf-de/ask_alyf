dea633f5b7a9068abf22d7fb97765ff2c3c44f9d
barredterra <14891507+barredterra@users.noreply.github.com>
2026-08-29 19:27:19 +0200

    fix: use frappe.utils.icon

--- ask_alyf/public/js/ask_alyf.bundle.js
+++ ask_alyf/public/js/ask_alyf.bundle.js
@@ -574,7 +574,8 @@ import "./field_agent";
 				copyButton.type = "button";
 				copyButton.title = label;
 				copyButton.setAttribute("aria-label", label);
-				copyButton.innerHTML = getIcon("copy", "xs", "", true);
+				copyButton.innerHTML =
+					typeof frappe.utils?.icon === "function" ? frappe.utils.icon("es-line-copy", "xs") : "Copy";
 				copyButton.addEventListener("click", () =>
 					frappe.utils.copy_to_clipboard(entry.copyText || ""),
 				);
